bbae1ed0f5921e8a2ef6e7866e7a6e89ac3b6490
Alexandr.Belotsitsko <sanya_belyy_0202@mail.ru>
2024-10-29 22:00:01 +0600

    data in ssr

--- local/js/vite/src/pages/test/App.vue
+++ local/js/vite/src/pages/test/App.vue
@@ -2,10 +2,19 @@
 import HelloWorld from './components/HelloWorld.vue'
 import img1 from '@/assets/images/11.png';
 import img2 from '/public/images/11.png';
+import { inject, ref } from 'vue';
+const msg = ref("Vite + Vue");
+
+const initialize = (data) => {
+  msg.value = data.msg
+}
+const initialData = inject('initialData');
+if (initialData && Object.keys(initialData).length !== 0) initialize(initialData);
+// pm2 restart node-server
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue1" />
+  <HelloWorld :msg="msg" />
   <div><img :src="img1" alt=""></div>
   <div><img :src="img2" alt=""></div>
 </template>
@@ -16,9 +25,11 @@ import img2 from '/public/images/11.png';
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
--- local/js/vite/src/pages/test/entry-client.ts
+++ local/js/vite/src/pages/test/entry-client.ts
@@ -1,6 +1,14 @@
 import './style.css'
 import { createApp } from './main'
 
-const { app } = createApp()
-
-app.mount('#app')
+let app;
+globalThis.vueApps = {
+    ...(globalThis.vueApps ?? {}),
+    
+    createTestPage(data?: Object) {
+        if(!app){
+            app = createApp(data);
+        }
+        return app;
+    }
+}
