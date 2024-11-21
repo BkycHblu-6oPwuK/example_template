397e397cba4f3a247707cbdce9cdbeb251a672ad
Alexandr Belotsitsko <sanyabelyy020@gmail.com>
2024-11-21 08:56:19 +0600

    remove hash name in output build

---
+++ local/js/vite/vite.config.client.js
@@ -0,0 +1,61 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueMacros from "unplugin-vue-macros/vite";
+import dotenv from 'dotenv';
+import path from 'path';
+
+const envPath = path.resolve(__dirname, '../../../local/php_interface/include/.env');
+const result = dotenv.config({ path: envPath });
+
+if (result.error) {
+  throw result.error;
+}
+
+const env = process.env;
+const base = env.MODE === 'production' ?  `/${env.VITE_BASE_PATH}/${env.VITE_CLIENT_PATH}` : `/${env.VITE_BASE_PATH}`;
+export default defineConfig({
+    plugins: [
+        VueMacros({
+            plugins: {
+                vue: vue(),
+            },
+        }),
+    ],
+    base: base,
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
+    },
+    build: {
+        outDir: env.VITE_CLIENT_PATH,
+        assetsDir: '.', 
+        copyPublicDir: false,
+        manifest: true,
+        rollupOptions: {
+            input: {
+                bundle: 'src/common/js/bundle.js',
+                header: 'src/common/js/header.js',
+                main: 'src/common/js/main.js',
+                footer: 'src/common/js/footer.js',
+                test: 'src/pages/test/entry-client.js'
+            },
+            output: {
+                entryFileNames: `[name].js`,
+                chunkFileNames: `[name].js`,
+                assetFileNames: `[name].[ext]`
+            }
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: env.VITE_PORT,
+        open: false,
+        cors: {
+            origin: '*'
+        },
+        hmr: {
+            host: 'localhost',
+        },
+    }
+});
